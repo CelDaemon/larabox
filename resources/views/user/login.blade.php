4de6212a0441296e878001448ab59f0fdc0cb1f1
CelDaemon <devoid@voidgroup.net>
2024-05-14 20:35:11 +0200

    Hide password in the login page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -2,8 +2,8 @@
     <link rel="stylesheet" href="/css/user/login.css">
     <x-single-modal title="Login" subtitle="Welcome to Larabox!">
         <form method="POST" class="single-modal-form">
-            <input name="email" placeholder="Email" type="email" required value="{{old("email")}}">
-            <input name="password" placeholder="Password" required value="{{old("password")}}">
+            <input type="email" name="email" placeholder="Email" required value="{{old("email")}}">
+            <input type="password" name="password" placeholder="Password" required value="{{old("password")}}">
             <input type="submit" value="Login">
         </form>
     </x-single-modal>
