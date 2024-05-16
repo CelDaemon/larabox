4b25bd4ac5b9a434a5377a587044e3045f30d37c
CelDaemon <devoid@voidgroup.net>
2024-05-17 01:08:11 +0200

    Remove reference to deleted PlaylistSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,5 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-
-        $this->callOnce([PlaylistSeeder::class], parameters: ["user" => $user]);
     }
 }
