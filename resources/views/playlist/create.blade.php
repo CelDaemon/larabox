91b9c188b7ec2b1c02b87121285a55f089a2625d
CelDaemon <devoid@voidgroup.net>
2024-05-17 01:05:31 +0200

    Keep old value in playlist creation page

--- resources/views/playlist/create.blade.php
+++ resources/views/playlist/create.blade.php
@@ -1,7 +1,7 @@
 <x-layout name="Create Playlist">
     <h1>Create Playlist</h1>
     <form method="POST" action="{{route("playlist.store")}}">
-        <label for="title">Title: </label><input id="title" name="title" type="text" required>
+        <label for="title">Title: </label><input id="title" name="title" type="text" required value="{{old("title")}}">
         <input type="submit">
     </form>
 </x-layout>
