36c0fe2eaa972872e4fa13da971127f083ee9563
CelDaemon <devoid@voidgroup.net>
2024-05-22 09:13:27 +0200

    Add to playlist button for songs

--- resources/views/song/index.blade.php
+++ resources/views/song/index.blade.php
@@ -4,9 +4,14 @@
     /** @var Collection<Song> $songs */
 @endphp
 <x-layout title="List Songs">
+    <style @nonce>
+        form {
+            display: inline;
+        }
+    </style>
     <ul>
         @foreach($songs as $song)
-            <li>{{$song->title}}</li>
+            <li>{{$song->title}} - <form><input type="hidden" name="song" value="{{$song->id}}"><input type="submit" value="Add to playlist"></input></form></li>
         @endforeach
     </ul>
 </x-layout>
