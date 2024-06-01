7b6e01fa860bfef45acaaaa4951d5101220c495b
CelDaemon <devoid@voidgroup.net>
2024-06-01 22:47:11 +0200

    Add component system support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
+import glob from "fast-glob";
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/layout.css', 'resources/ts/app.ts'],
+            input: [...glob.sync("resources/css/components/**/*.css"), 'resources/ts/app.ts'],
             refresh: true,
         }),
     ],
+    build: {
+        target: "ESNext"
+    },
 });
