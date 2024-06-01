cb6d7640766e82e8b5fa3a047d9078eec959287c
CelDaemon <celdaemon@voidgroup.net>
2024-06-01 22:47:11 +0200

    Add component system support

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
 <head lang="en">
     <title>@if($title !== null){{$title}} - @endif{{config("app.name")}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    @vite(["resources/ts/app.ts", "resources/css/layout.css"])
+    @vite(["resources/ts/app.ts", "resources/css/components/layout.css"])
 </head>
 <body>
 {{$slot}}
