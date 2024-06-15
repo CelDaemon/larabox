5b91a94ff652fc828fbcbe5d0111013eea817b80
CelDaemon <devoid@voidgroup.net>
2024-06-15 23:29:08 +0200

    Add Content Security Policy

---
+++ app/Http/Middleware/ContentSecurityPolicyMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Foundation\Http\Middleware\Concerns\ExcludesPaths;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Vite;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContentSecurityPolicyMiddleware
+{
+    use ExcludesPaths;
+
+    protected array $except = ["/up"];
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $nonce = Vite::useCspNonce();
+        $request->attributes->set('nonce', $nonce);
+        $response = $next($request);
+        if($this->inExceptArray($request) || $response->isClientError() || $response->isServerError()) return $response;
+        $response->headers->set('Content-Security-Policy', "script-src 'strict-dynamic' 'nonce-$nonce';style-src 'self' 'nonce-$nonce';object-src 'none';base-uri 'none';frame-ancestors 'none'", false);
+        return $response;
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,21 @@
     @guest
         <a href='{{route('login')}}'>Login</a> <a href='{{route('register')}}'>Register</a>
     @else
+        <style @nonce>
+            .not-verified {
+                color: red;
+            }
+        </style>
         @php
             /** @var User $user */
             $user = Auth::user();
         @endphp
         <h1>Home</h1>
         @if(!$user->hasVerifiedEmail())
-            <h4 style="color: red;">Your email is not verified, if the email didn't arrive, <a href="{{route('verification.notice')}}">resend it here.</a></h4>
+            <h4 class="not-verified">Your email is not verified, if the email didn't arrive, <a href="{{route('verification.notice')}}">resend it here.</a></h4>
         @endif
         <form action="{{route('logout')}}" method="POST">
+            @method("DELETE")
             <input type="submit" value="Logout">
         </form>
     @endguest
