c0a86ad285c0061a0928235824fc79cca7f3fa7b
CelDaemon <devoid@voidgroup.net>
2024-06-17 22:45:38 +0200

    Change usage of translation, as intended by laravel docs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,20 @@
-<x-layout :title="__('login')">
-    <h1>{{__('login')}}</h1>
+<x-layout :title="__('Login')">
+    <h1>{{__('Login')}}</h1>
     <form method="POST" action="{{route('auth.login')}}" novalidate>
         <div>
-            <label for="email">{{__('email')}}:</label>
+            <label for="email">{{__('Email')}}:</label>
             <input id="email" type="text" name="email" maxlength="255">
         </div>
         @error('email')
             <div>{{$message}}</div>
         @enderror
         <div>
-            <label for="password">{{__('password')}}:</label>
+            <label for="password">{{__('Password')}}:</label>
             <input id="password" type="password" name="password" maxlength="255">
         </div>
         @error('password')
             <div>{{$message}}</div>
         @enderror
-        <input type="submit" value="{{__('login')}}">
+        <input type="submit" value="{{__('Login')}}">
     </form>
 </x-layout>
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,33 +1,33 @@
 @use('Illuminate\Support\Facades\Auth')
 @use('App\Models\User')
 @php(/** @var User $user */ $user = Auth::user())
-<x-layout :title="__('settings')">
-    <h1>{{__('settings')}}</h1>
+<x-layout :title="__('Settings')">
+    <h1>{{__('Settings')}}</h1>
     <form method="POST" action="{{route('users.update', ['user' => $user])}}" novalidate>
         @method("PATCH")
         <div>
-            <label for="name">{{__('name')}}:</label>
+            <label for="name">{{__('Name')}}:</label>
             <input id="name" type="text" name="name" maxlength="255" value="{{old('name') ?? $user->name}}">
         </div>
         @error('name')
             <div>{{$message}}</div>
         @enderror
         <div>
-            <label for="email">{{__('email')}}:</label>
+            <label for="email">{{__('Email')}}:</label>
             <input id="email" type="text" name="email" maxlength="255" value="{{old('email') ?? $user->email}}">
         </div>
         @error('email')
         <div>{{$message}}</div>
         @enderror
-        <input type="submit" value="{{__('update')}}">
+        <input type="submit" value="{{__('Update')}}">
     </form>
     <form method="POST" action="{{route('auth.logout')}}">
-        <input type="submit" value="{{__('logout')}}">
+        <input type="submit" value="{{__('Logout')}}">
     </form>
     <hr>
-    <h3>{{__('settings.delete_warning')}}</h3>
+    <h3>{{__('Be warned, your account cannot be recovered!')}}</h3>
     <form method="POST" action="{{route('users.destroy', ['user' => $user])}}">
         @method("DELETE")
-        <input type="submit" value="{{__('delete')}}">
+        <input type="submit" value="{{__('Delete')}}">
     </form>
 </x-layout>
