ce8bf6e06e0facdff1e2fd11df04b2bc277101f7
CelDaemon <celdaemon@voidgroup.net>
2024-06-17 11:21:18 +0200

    Rework routes

--- routes/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Admin\BetaController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('can:admin')->prefix('/admin')->group(function () {
-    Route::get('/beta', BetaController::class)->name('admin.beta');
-});
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthenticatedSessionController;
-use App\Http\Controllers\EmailVerificationController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\SettingsController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
-Route::get('/register', [RegisterController::class, 'create'])->name('register');
-Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-Route::get('/settings', SettingsController::class)->middleware('auth')->name('settings');
-
-Route::patch('/users/{user}/beta', [UserController::class, "updateBeta"])->middleware('can:admin')->name('users.update.beta');
-Route::apiResource('users', UserController::class)->only(['update', 'destroy']);
-
-Route::get('/verify', [EmailVerificationController::class, 'index'])->middleware('auth')->name('verification.notice');
-Route::post('/verify', [EmailVerificationController::class, 'send'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-Route::get('/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware(['auth', 'signed'])->name('verification.verify');
-require __DIR__.'/beta.php';
-require __DIR__.'/admin.php';
 require __DIR__.'/debug.php';
