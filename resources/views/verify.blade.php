1ae78aa615e397df1a6945e7c320ac08880f7f84
CelDaemon <devoid@voidgroup.net>
2024-06-17 11:21:18 +0200

    Rework routes

--- resources/views/verify.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout title="Verify">
-    <h1>Your email is not verified</h1>
-    <form method="POST" action="{{route('verification.send')}}">
-        <input type="submit" value="Send verification email">
-    </form>
-    {{session('message')}}
-</x-layout>
--- routes/beta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('can:beta')->group(function () {
-    Route::get('test', function () {
-        return 'Example beta route!';
-    })->middleware('verified');
-});
