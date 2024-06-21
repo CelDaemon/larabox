e35a0f114363b65c2efc783f0dbc409c799a50d2
CelDaemon <devoid@voidgroup.net>
2024-06-22 01:38:37 +0200

    Don't show playlist edit button without update permission

--- resources/views/playlists/show.blade.php
+++ resources/views/playlists/show.blade.php
@@ -3,5 +3,7 @@
 <x-layout :title="$playlist->title">
     <h1>{{$playlist->title}}</h1>
     <a href="{{route('playlists.index')}}">{{__('Back')}}</a>
-    <a href="{{route('playlists.edit', ['playlist' => $playlist])}}">{{__('Edit')}}</a>
+    @can('update', $playlist)
+        <a href="{{route('playlists.edit', ['playlist' => $playlist])}}">{{__('Edit')}}</a>
+    @endcan
 </x-layout>
