4b7202a56a8453d6513ceea006dbccca67f40360
CelDaemon <celdaemon@voidgroup.net>
2024-06-23 22:35:31 +0200

    Add basic tests for up route and playlist creation

---
+++ tests/Feature/UpTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class UpTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_the_up_route_returns_a_successful_response(): void
+    {
+        $this->get('/up')->assertOk();
+    }
+}
