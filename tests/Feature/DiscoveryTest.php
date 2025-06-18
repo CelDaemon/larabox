207e05dea8c5d0c9484915295511124542fc7e47
Celeste <celdaemon@voidgroup.net>
2025-06-18 11:42:36 +0200

    Refresh database in discovery test

--- tests/Feature/DiscoveryTest.php
+++ tests/Feature/DiscoveryTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DiscoveryTest extends TestCase
 {
+    use RefreshDatabase;
+    
     public function test_discovery_screen_can_be_rendered()
     {
         $this->get(route('discovery'))->assertSuccessful();
