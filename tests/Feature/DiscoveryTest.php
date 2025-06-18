3491ad9e0f53d4b9d6e688022ee42db5de16d20c
CelDaemon <celdaemon@voidgroup.net>
2025-06-18 12:20:34 +0200

    Ensure discovery test uses seeded database

--- tests/Feature/DiscoveryTest.php
+++ tests/Feature/DiscoveryTest.php
@@ -8,9 +8,10 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class DiscoveryTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_discovery_screen_can_be_rendered()
     {
+    	$this->seed();
         $this->get(route('discovery'))->assertSuccessful();
     }
 }
